5194b5cfc91de7577c7e5a5149212ae72e7ac0b8
glencurran <glencurran@eircom.net>
2017-11-30 16:52:26 +0000

    Attempted to get checkMatch() working but it still either doesn't recognize a match and turns over both cards or it thinks every pair is a match.

--- src/secondYearProject/GameBoard.java
+++ src/secondYearProject/GameBoard.java
@@ -31,7 +31,9 @@ public class GameBoard extends JFrame implements ActionListener{
     int score = 0;
     Player player1;
 
-
+    /**
+     * GameBoard constructor builds the UI using a JFrame. It adds multiple JComponents on to this panel.
+     */
     public GameBoard(){
         setTitle("Concentration Card Game");
         setPreferredSize(new Dimension(600, 800));
@@ -39,7 +41,7 @@ public class GameBoard extends JFrame implements ActionListener{
         setResizable(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        /**
+        /*
          * Building an array of images, Using each 1 twice so they can match.
          * Cheated and hard coded the values, I was originally using a for loop to generate this but I was
          * running into a lot of problems and needed to simplify my code.
@@ -53,13 +55,16 @@ public class GameBoard extends JFrame implements ActionListener{
         images[6] = new ImageIcon(this.getClass().getResource("\\images\\4.png"));
         images[7] = new ImageIcon(this.getClass().getResource("\\images\\4.png"));
 
+        /*
+         *sets the start image to the back of the card.
+         */
         turnedOver = new ImageIcon(this.getClass().getResource("\\images\\cardImage.png"));
         gamePanel.setPreferredSize(new Dimension(500, 450));
         gamePanel.setLayout(new GridLayout(2, images.length));
         numButtons = images.length;
         buttons = new JButton[numButtons];
 
-        /**
+        /*
          * loop used to populate the game board with cards/buttons.
          */
         for(int i = 0; i < images.length; i++){
@@ -69,7 +74,7 @@ public class GameBoard extends JFrame implements ActionListener{
             gamePanel.add(buttons[i]);
         }
         /**
-         * adding various jComponents to the lower JPanel.
+         * JLabel used to display instructions to the player
          */
         JLabel nameLabel = new JLabel("Player Name:");
         Font font1 = new Font("SansSerif", Font.BOLD, 25);
@@ -77,12 +82,18 @@ public class GameBoard extends JFrame implements ActionListener{
         nameLabel.setForeground(Color.BLACK);
         nameLabel.setBounds(20, 0, 250, 50);
 
+        /**
+         * JLabel used to display the player name, wanted it to invoke player.getName() but ran out of time.
+         */
         JLabel nameFromMemory = new JLabel("\"Placeholder for player name\""); // player1.getName();
         font1 = new Font("SansSerif", Font.BOLD, 25);
         nameFromMemory.setFont(font1);
         nameFromMemory.setForeground(Color.RED);
         nameFromMemory.setBounds(270, 0, 250, 50);
 
+        /**
+         * JLabel used to display instructions to the player
+         */
         JLabel scoreLabel = new JLabel();
         scoreLabel.setText("Score:");
         font1 = new Font("SansSerif", Font.BOLD, 25);
@@ -90,12 +101,18 @@ public class GameBoard extends JFrame implements ActionListener{
         scoreLabel.setForeground(Color.BLACK);
         scoreLabel.setBounds(20, 60, 250, 50);
 
+        /**
+         * JLabel used to store and display the score.
+         */
         scoreCounterLabel = new JLabel("" + score);
         font1 = new Font("SansSerif", Font.BOLD, 25);
         scoreCounterLabel.setFont(font1);
         scoreCounterLabel.setForeground(Color.RED);
         scoreCounterLabel.setBounds(270, 60, 250, 50);
 
+        /**
+         * JButton used to restart the game, resets the game board.
+         */
         restart = new JButton(new ImageIcon(this.getClass().getResource("\\images\\Restart.png")));
         restart.addActionListener(this);
         restart.setBounds(20, 120, 250, 50);
@@ -108,7 +125,8 @@ public class GameBoard extends JFrame implements ActionListener{
         start.add(scoreCounterLabel);
         start.add(restart);
 
-        /**
+
+        /*
          * shuffleCards() called statically to rearrange the deck.
          */
         shuffleCards(images);
@@ -124,7 +142,8 @@ public class GameBoard extends JFrame implements ActionListener{
     }
     /**
      * Shuffle method uses java.util.Random to generate a random number between 0 and 8(numButtons).
-     * It takes an array of ImageIcons and rearranges them.
+     * It takes an array of ImageIcons and rearranges them using a placeholder to store the image and put it back
+     * into the image array in the place of i.
      *
      */
     public  static void shuffleCards(ImageIcon[] images){
@@ -138,11 +157,26 @@ public class GameBoard extends JFrame implements ActionListener{
     }
 
     /**
-     * checkMatch() takes in two JButtons are arguments and then checks to see if their references match. If they do
-     * it returns true and if they don't it returns false.
+     * checkMatch maethod takes in two JButtons are arguments and then checks to see if their references match.
+     * If they do, it returns true and if they don't it returns false.
+     */
+    /*
+     * Cureently not functioning properly. I've tried multiple options but ran out of time to get it fixed.
      */
     public static boolean checkMatch(JButton button1, JButton button2){
-        if(button1.getIcon().equals( button2.getIcon())){
+//        if((button1.getIcon().getClass().getResource("\\images\\1.png") == button2.getIcon().getClass().getResource("\\images\\1.png")) ||
+//                (button1.getIcon().getClass().getResource("\\images\\2.png").equals(button2.getIcon().getClass().getResource("\\images\\2.png"))) ||
+//                (button1.getIcon().getClass().getResource("\\images\\3.png").equals(button2.getIcon().getClass().getResource("\\images\\3.png"))) ||
+//                (button1.getIcon().getClass().getResource("\\images\\4.png").equals(button2.getIcon().getClass().getResource("\\images\\4.png")))){
+//            return true;
+//        }else if (button1.getIcon().getClass().getResource("\\images\\2.png").equals(button2.getIcon().getClass().getResource("\\images\\2.png"))){
+//            return true;
+//        }else if(button1.getIcon().getClass().getResource("\\images\\3.png").equals(button2.getIcon().getClass().getResource("\\images\\3.png"))){
+//            return true;
+//        }else if(button1.getIcon().getClass().getResource("\\images\\4.png").equals(button2.getIcon().getClass().getResource("\\images\\4.png"))){
+//            return true;
+
+        if(button1.getIcon() == button2.getIcon()){
             return true;
         }
         return false;
@@ -161,11 +195,12 @@ public class GameBoard extends JFrame implements ActionListener{
                 if(numClicks == 1){
                     buttons[i].setIcon(images[i]);
                     cardHolder1 = i;
+                    System.out.println(cardHolder1);
                 }else if(numClicks == 2){
                     buttons[i].setIcon(images[i]);
                     cardHolder2 = i;
-                }
-                if(numClicks == 3){
+                    System.out.println(cardHolder2);
+                }else if(numClicks == 3){
                     if(checkMatch(buttons[cardHolder1], buttons[cardHolder2])){
                         score++;
                         scoreCounterLabel.setText("" + score);
@@ -176,7 +211,6 @@ public class GameBoard extends JFrame implements ActionListener{
                         numClicks = 0;
                     }
                 }
-
                 if(Integer.parseInt(scoreCounterLabel.getText()) == 4){
                     JOptionPane.showMessageDialog(null, "Congratulations, You Won");
                 }
